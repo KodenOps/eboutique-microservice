391a88e00ac134e2181b80b9f2bc5bb427e87314
kodenops <segun.femifadiya@gmail.com>
2025-05-23 15:01:36 +0100

    triggers the build and push action

--- src/cartservice/src/Program.cs
+++ src/cartservice/src/Program.cs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+// #
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using cartservice;
@@ -24,3 +24,4 @@ static IHostBuilder CreateHostBuilder(string[] args) =>
         {
             webBuilder.UseStartup<Startup>();
         });
+
